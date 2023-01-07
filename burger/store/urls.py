e9ce22000bfcb99cd10c46b244ccad787db87e9d
BabintsevVO <babintsevvo@gmail.com>
2023-01-07 18:08:22 +0300

    add block burger premium (sort by rating)

--- burger/store/urls.py
+++ burger/store/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', ProductByCategory.as_view(), name='category'),
     path('product/<str:slug>/', get_product, name='product'),
 ]
--- burger/store/views.py
+++ burger/store/views.py
@@ -12,17 +12,31 @@ class Home(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Burgers'
+        context['premiums'] = Product.objects.all().order_by('-rating')[:2]
         return context
 
 
+class ProductByCategory(ListView):
+    template_name = 'store/index.html'
+    context_object_name = 'products'
+    paginate_by = 3
+    allow_empty = False
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = ProductCategory.objects.get(slug=self.kwargs['slug'])
+        return context
 
 def index(request):
     return render(request, 'store/index.html')
 
 
 def get_category(request):
-    return render(request, 'store/menu.html')
+    return render(request, 'store/index.html')
 
 
 def get_product(request):
-    return render(request, 'store/menu.html')
+    return render(request, 'store/index.html')
