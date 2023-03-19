50bb81f1d027d4543b15d262c3e237ff8711f4d6
BabintsevVO <babintsevvo@gmail.com>
2023-03-19 19:19:43 +0300

    make settings package

--- burger/burger/settings/base.py
+++ burger/burger/settings/base.py
@@ -63,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, '../templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'burger.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -126,12 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'burger/static'),
+    os.path.join(BASE_DIR, '../burger/static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/users/login/'
